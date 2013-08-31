9b88edac2ebc299df660a575b5b8180ab3932ce3
Aaron Baker <aaron.baker@neo.com>
2013-08-31 12:59:20 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BocaChondor::Application.config.secret_key_base = 'f358f1d1e9be4c59b6db87f5039b415d3c748ce3387e1d0f7bfc884802a733bf1c05a9e8aae28d9869bd12d9135109716ca44f6f609c5b25f267b63ea4f4356a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BocaChondor::Application.config.session_store :cookie_store, key: '_boca_chondor_session'
